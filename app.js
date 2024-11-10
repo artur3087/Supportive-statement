b13ffa11b9dcbd527fac6879c9382aa745b47d49
artur3087 <artur3087@gmail.com>
2024-11-10 20:44:23 +0000

    changes_pack1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const card = document.querySelector(".card");
 const btn1 = document.querySelector(".btn1");
 
-card.addEventListener("click", () => {
-    card.classList.toggle("flipCard");
-})
+// Optional function to flip the card below//
+
+// card.addEventListener("click", () => {
+//     card.classList.toggle("flipCard");
+// })
 
 btn1.addEventListener("click", () => {
     card.classList.toggle("flipCard");
